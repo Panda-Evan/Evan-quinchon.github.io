5f8aa8ef270a194016895f3553083a3af26ca60a
Evan Quinchon <122290394+Panda-Evan@users.noreply.github.com>
2023-01-18 09:44:47 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath:"/Panda-Evan.github.io"
+  basePath:"/Panda-Evan"
 }
 
 module.exports = nextConfig
