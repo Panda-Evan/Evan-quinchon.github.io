71d70a485951bf06c49d405eafd719ea43faa10c
Evan QUINCHON <quinch_e@etna-alternance.io>
2023-03-13 01:34:12 +0100

    add url comp

--- components/comp.tsx
+++ components/comp.tsx
@@ -9,31 +9,37 @@ export default function Comp() {
       label: "Developement Web",
       content: [
         {
+          url:"https://nextjs.org/",
           src: "/logo_prog/web/nextjs.png",
           label: "NextJS",
           badgeClass: styles.badge_black,
         },
         {
+          url:"https://fr.reactjs.org/",
           src: "/logo_prog/web/react.webp",
           label: "ReactJS",
           badgeClass: styles.badge_blue_light,
         },
         {
+          url:"https://developer.mozilla.org/fr/docs/Web/CSS",
           src: "/logo_prog/web/css.png",
           label: "CSS",
           badgeClass: styles.badge_blue,
         },
         {
+          url:"https://sass-lang.com/",
           src: "/logo_prog/web/scss.png",
           label: "SCSS",
           badgeClass: styles.badge_pink,
         },
         {
+          url:"https://developer.mozilla.org/fr/docs/Web/JavaScript",
           src: "/logo_prog/web/js.png",
           label: "Javascript",
           badgeClass: styles.badge_yellow,
         },
         {
+          url:"https://flutter.dev/",
           src: "/logo_prog/web/flutter.png",
           label: "Flutter",
           badgeClass: styles.badge_bleu_ciel,
@@ -44,21 +50,25 @@ export default function Comp() {
       label: "Language de programmation",
       content: [
         {
+          url:"https://www.python.org/",
           src: "/logo_prog/prog/python.png",
           label: "Python",
           badgeClass: styles.badge_purple,
         },
         {
+          url:"https://www.ruby-lang.org/fr/",
           src: "/logo_prog/prog/ruby.png",
           label: "Ruby",
           badgeClass: styles.badge_red,
         },
         {
+          url:"https://fr.wikipedia.org/wiki/C_(langage)",
           src: "/logo_prog/prog/C.png",
           label: "C",
           badgeClass: styles.badge_blue,
         },
         {
+          url:"https://www.lua.org/",
           src: "/logo_prog/prog/lua.png",
           label: "Lua",
           badgeClass: styles.badge_blue_dark,
@@ -69,11 +79,13 @@ export default function Comp() {
       label: "Autres",
       content: [
         {
+          url:"https://git-scm.com/",
           src: "/logo_prog/other/git.png",
           label: "Git",
           badgeClass: styles.badge_orange,
         },
         {
+          url:"https://www.figma.com/fr/",
           src: "/logo_prog/other/figma.png",
           label: "Figma",
           badgeClass: styles.badge_green,
@@ -106,7 +118,7 @@ export default function Comp() {
               <a
                 key={index}
                 target="_blank"
-                href="#"
+                href={content.url}
                 className={content.badgeClass}
               >
                 <div className={styles.circle}>
